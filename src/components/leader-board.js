9eacdcd6d150b4c7bead0e5a11819155f13b9cbe
esraa.afifi <esraa.mohamed87@gmail.com>
2020-01-06 18:47:45 -0800

    adding leader board component

--- src/components/leader-board.js
+++ src/components/leader-board.js
@@ -1,9 +1,55 @@
-import React, { Component } from 'react'
+import React, {Component, Fragment} from 'react'
+import {Row, Card, Col, Avatar, Divider} from "antd";
 
-class LeaderBoard extends Component{
+class LeaderBoard extends Component {
+
+    getAnsweredQuestionsCount = (answers) => {
+        return Object.keys(answers).length
+    }
+
+    getCreatedQuestionsCount = (questions) => {
+        return questions.length
+    }
+
+    getScore = (answers, questions) => {
+        return this.getAnsweredQuestionsCount(answers) + this.getCreatedQuestionsCount(questions)
+    }
+
     render() {
+        const {users} = this.props
+        console.log(users)
+
         return (
-            <div>Leader Board</div>
+            <Fragment>
+                {users && Object.values(users).sort((a, b) => (Object.keys(b.answers).length + b.questions.length) - (Object.keys(a.answers).length + a.questions.length))
+                    .map((user) => <Row key={user.id}>
+                            <Card className='question-card'>
+                                <Row span={24}>
+                                    <Col span={6}>
+                                        <Avatar className='leader-board-avatar' src={user.avatarURL}/>
+                                    </Col>
+                                    <Col span={1}>
+                                        <Divider type='vertical' className='vl'/>
+                                    </Col>
+                                    <Col span={10}>
+                                        <Row className='leader-board-header'>{user.name}</Row>
+                                        <Row className='questions-scoring'>Answered Questions {this.getAnsweredQuestionsCount(user.answers)}</Row>
+                                        <Divider type='horizontal' className='hl'/>
+                                        <Row className='questions-scoring'>Created Questions {this.getCreatedQuestionsCount(user.questions)}</Row>
+                                    </Col>
+                                    <Col span={1}>
+                                        <Divider type='vertical' className='vl'/>
+                                    </Col>
+                                    <Col span={6}>
+                                        <Row className='score-header'>Score</Row>
+                                        <Row className='score-result'>{this.getScore(user.answers, user.questions)}</Row>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Row>
+                    )
+                }
+            </Fragment>
         )
     }
 }
